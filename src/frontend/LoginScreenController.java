bfb479e60bcb6859d68231a3b12b5130abf25dcb
emily-honarchian <em.honarchian@gmail.com>
2026-06-30 16:17:16 -0700

    Created login screen fxml & controller that will show login screen with username & password, and created a main page fxml that is waiting to be built

---
+++ src/frontend/LoginScreenController.java
@@ -0,0 +1,38 @@
+package frontend;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import java.io.IOException;
+
+public class LoginScreenController {
+    
+    @FXML
+    private TextField userName;
+
+    @FXML
+    private PasswordField userPassword;
+
+    private Parent root;
+    private Stage stage;
+    private Scene scene;
+    @FXML
+    public void loginUser(ActionEvent event) throws IOException{
+        String username = userName.getText();
+        String password = userPassword.getText();
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainPage.fxml"));
+        root = loader.load();
+
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+        
+    }
+
+
+}
