d446b76885fd9f582a90533d21e53f724c27924c
emily-honarchian <em.honarchian@gmail.com>
2026-06-30 21:54:05 -0700

    created UI for search/navigation screen for available cars

--- src/frontend/CartViewController.java
+++ src/frontend/CartViewController.java
@@ -14,7 +14,7 @@ import javafx.stage.Stage;
 public class CartViewController {
     
     @FXML
-    private TableView userCartTableView;
+    private TableView<String> userCartTableView;
 
     private Parent root;
     private Stage stage;
