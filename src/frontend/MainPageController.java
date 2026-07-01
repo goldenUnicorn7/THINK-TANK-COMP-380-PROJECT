23df4da083dc540d0efb0f0aa5f33fe3c26b9160
emily-honarchian <em.honarchian@gmail.com>
2026-06-30 17:50:54 -0700

    created loaders for the SearchView screen and AvailableCarsView screen, which will load the respective screen upon clicking its button on the main page

--- src/frontend/MainPageController.java
+++ src/frontend/MainPageController.java
@@ -2,6 +2,11 @@ package frontend;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+
+import java.io.IOException;
+
+import org.w3c.dom.Node;
+
 import javafx.event.ActionEvent;
 
 public class MainPageController {
@@ -32,6 +37,32 @@ public class MainPageController {
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    @FXML
+    public void goToSearch(ActionEvent event) throws IOException{
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("SearchView.fxml"));
+
+        root = loader.load();
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void goToAvailableCars(ActionEvent event) throws IOException{
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("AvailableCarsView.fxml"));
+
+        root = loader.load();
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+
     }
 
 
