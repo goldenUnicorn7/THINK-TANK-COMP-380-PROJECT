20e819bcbd2a4a84c96009d3d62d66948e98d07d
emily-honarchian <em.honarchian@gmail.com>
2026-06-30 21:29:42 -0700

    updated CartViewController to allow scene change back to main menu from user's cart. also added jdk 26 to settings.json in order to import libraries

--- src/frontend/CartViewController.java
+++ src/frontend/CartViewController.java
@@ -1,5 +1,36 @@
 package frontend;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+
 public class CartViewController {
     
+    @FXML
+    private TableView userCartTableView;
+
+    private Parent root;
+    private Stage stage;
+    private Scene scene;
+
+    @FXML
+    public void goToMainMenu(ActionEvent event) throws IOException{
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainPage.fxml"));
+        root = loader.load();
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+
+    }
+
 }
