163784d8e703e6822a97e49c4e06a29e14f0cb72
emily-honarchian <em.honarchian@gmail.com>
2026-06-30 22:01:41 -0700

    added option for user to go back to main page from search screen

--- src/frontend/SearchViewController.java
+++ src/frontend/SearchViewController.java
@@ -1,5 +1,36 @@
 package frontend;
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class SearchViewController {
+    @FXML
+    private TextField searchBar;
+
+    @FXML
+    private TableView searchResults;
+
+    private Parent root;
+    private Stage stage;
+    private Scene scene;
+
+    @FXML
+    public void goToMainPage(ActionEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainPage.fxml"));
+        root = loader.load();
+        stage= (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+
     
 }
