e8e655c0bf668eea466c2463b33a2a9663904590
emily-honarchian <em.honarchian@gmail.com>
2026-07-12 11:55:12 -0700

    added available cars screen that will show list of cars

---
+++ src/main/java/frontend/AvailableCarsController.java
@@ -0,0 +1,24 @@
+package frontend;
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+
+
+public class AvailableCarsController {
+    
+    private Parent root;
+    private Stage stage;
+    private Scene scene;
+
+
+
+
+}
